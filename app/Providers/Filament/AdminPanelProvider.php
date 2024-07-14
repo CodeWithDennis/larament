34a832927fe563ccd0ff9b930cfa174d73ab392b
Dennis Elsinga <personal.elsinga@gmail.com>
2024-07-14 20:42:11 +0200

    Add user registration feature to admin panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login(Login::class)
+            ->registration()
             ->colors([
                 'primary' => Color::Amber,
             ])
