5bd4fc5c3e8241cb30dc067690ff4485e97839e9
Dennis Elsinga <personal.elsinga@gmail.com>
2024-07-15 15:41:20 +0200

    Add admin theme

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -33,6 +33,7 @@ class AdminPanelProvider extends PanelProvider
 //            ->sidebarFullyCollapsibleOnDesktop()
             ->spa()
             ->profile(Profile::class, false)
+            ->viteTheme('resources/css/filament/admin/theme.css')
             ->colors([
                 'primary' => Color::Amber,
             ])
