2aa09fede1e67df09488ca7111ff65b81a5ab870
Dennis Elsinga <personal.elsinga@gmail.com>
2024-09-05 13:49:54 +0200

    Add custom helper function and autoload in composer.json

---
+++ app/Helpers.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * Here you can define your own helper functions.
+ * Make sure to use the `function_exists` check to not declare the function twice.
+ */
+
+if (! function_exists('example')) {
+    function example(): string
+    {
+        return 'This is an example function you can use in your project.';
+    }
+}
