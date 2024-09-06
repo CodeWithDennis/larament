e452dbe1531cd9ff9da83d2356e30b58fbfabbf4
Dennis Elsinga <personal.elsinga@gmail.com>
2024-09-06 17:35:53 +0200

    Implement logic for canAccessPanel method

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,6 +49,7 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
+        /* TODO: Please implement your own logic here. */
         return str_ends_with($this->email, '@example.com');
     }
 }
