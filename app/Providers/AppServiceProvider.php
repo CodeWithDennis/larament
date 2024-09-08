8abe2d475fc1028ad8cc761be80c09ef98b1a682
Dennis Elsinga <personal.elsinga@gmail.com>
2024-09-08 14:27:35 +0200

    Update Model::shouldBeStrict() call in boot method

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -37,7 +37,7 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        Model::shouldBeStrict();
+        Model::shouldBeStrict(! app()->isProduction());
 
         $this->translatableComponents();
     }
