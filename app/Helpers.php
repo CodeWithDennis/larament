85aa518a8093bc12d5a0f9eb6c4a2bb65db5ba02
Dennis Elsinga <personal.elsinga@gmail.com>
2024-09-09 10:57:13 +0200

    Testing

--- app/Helpers.php
+++ app/Helpers.php
@@ -5,7 +5,7 @@
  * Make sure to use the `function_exists` check to not declare the function twice.
  */
 
-if (! function_exists('example')) {
+if (!function_exists('example')) {
     function example(): string
     {
         return 'This is an example function you can use in your project.';
