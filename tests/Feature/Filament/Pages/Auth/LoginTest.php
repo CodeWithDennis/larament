d541d17ac2fcda6c21a038f13a8b1bdd45dd12c2
Dennis Elsinga <personal.elsinga@gmail.com>
2024-10-30 13:43:46 +0100

    Refactor authentication tests and setup method

--- tests/Feature/Filament/Pages/Auth/LoginTest.php
+++ tests/Feature/Filament/Pages/Auth/LoginTest.php
@@ -1,37 +1,28 @@
 <?php
 
-use App\Models\User;
 use Filament\Facades\Filament;
 
 test('an unauthenticated user can access the login page', function () {
+    auth()->logout();
+
     $this->get(Filament::getLoginUrl())
         ->assertOk();
 });
 
 test('an unauthenticated user can not access the admin panel', function () {
+    auth()->logout();
+
     $this->get('admin')
         ->assertRedirect(Filament::getLoginUrl());
 });
 
 test('an authenticated user can access the admin panel', function () {
-    $user = User::factory()->create([
-        'name' => 'John Doe',
-        'email' => 'john@example.com',
-    ]);
-
-    $this->actingAs($user)
-        ->get('admin')
+    $this->get('admin')
         ->assertOk();
 });
 
 test('an authenticated user can logout', function () {
-    $user = User::factory()->create([
-        'name' => 'John Doe',
-        'email' => 'john@example.com',
-    ]);
-
-    $this->actingAs($user)
-        ->get('admin')
+    $this->get('admin')
         ->assertOk();
 
     $this->post('admin/logout')
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,20 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->actingAs(User::factory()->create([
+            'name' => 'John Doe',
+            'email' => 'john@example.com',
+        ]));
+
+        $this->withoutVite();
+    }
 }
